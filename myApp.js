e036005efb4791f5c76cbc594d3353dbe92286d1
Albert Tagaban <albert.tagaban@gmail.com>
2023-08-03 15:30:21 +0800

    Update to load css assets

--- myApp.js
+++ myApp.js
@@ -1,25 +1,19 @@
 let express = require('express');
 let app = express();
 
+//define asset folder
+assets = express.static( __dirname +  "/public");
+// load asset folder
+app.use('/public',assets); 
 
 app.get('/',(req, res) => {
-//    res.send("Hello Express");
-    absolutePath = __dirname + '/views/index.html';
+
+    //    res.send("Hello Express");
+
+
+    absolutePath = __dirname + '/public';
     res.sendFile(absolutePath);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
