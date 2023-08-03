e38a45088dc4551ffbcfb7d1efab2238d8d74c89
Albert Tagaban <albert.tagaban@gmail.com>
2023-08-03 14:55:05 +0800

    Update to route content to index

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 let express = require('express');
 let app = express();
+//console.log("Hello World");
+
+function getIndex(req,res) {
+    res.send('Hello Express');
+}
 
 
-console.log("Hello World");
-
-
-
-
+app.get('/','getIndex');
 
 
 
