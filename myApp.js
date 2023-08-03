7d41db3b642d89ebbeed961e965e52b0ca5d80ed
Albert Tagaban <albert.tagaban@gmail.com>
2023-08-03 15:43:00 +0800

    Update to include json

--- myApp.js
+++ myApp.js
@@ -6,11 +6,20 @@ assets = express.static( __dirname +  "/public");
 // load asset folder
 app.use('/public',assets); 
 
+
+//load api
+
+app.get('/json',(req,res) => {
+    res.json({
+        message: "Hello json"
+    });
+});
+
+
+
+// load html
 app.get('/',(req, res) => {
-
     //    res.send("Hello Express");
-
-
     absolutePath = __dirname + '/public';
     res.sendFile(absolutePath);
 });
